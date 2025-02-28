e9a52ee5855334a3e203fcf9d8d707f755e82323
Kasidit Bualoeng <subdit@gmail.com>
2025-02-28 10:18:55 -0500

    add image on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import Image from 'next/image';
+
 export default function Home() {
-  return <div>Home Page!</div>;
+  return (
+    <div>
+      Home Page!
+      <img
+        src='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
+        alt='Google Logo'
+      />
+      <Image src='/home.jpg' alt='Home Image' width={500} height={500} />
+    </div>
+  );
 }
